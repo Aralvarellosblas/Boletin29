374e37176a9542672ef50a4d9e05e3f2163dd365
arturo <arturo@ArturoMSI>
2018-05-16 08:54:48 +0200

    Ejercicio Terminado

---
+++ Ejercicio29/src/ejercicio29/Veleros.java
@@ -0,0 +1,36 @@
+package ejercicio29;
+
+/**
+ *
+ * @author Arturo
+ */
+public class Veleros extends Barco{
+
+    private int mastiles;
+
+    public Veleros(){
+    }
+
+    public Veleros(int mastiles, int eslora, String matricula){
+        super(eslora, matricula);
+        this.mastiles=mastiles;
+    }
+
+    public int getMastiles(){
+        return mastiles;
+    }
+
+    public void setMastiles(int mastiles){
+        this.mastiles=mastiles;
+    }
+
+    @Override
+    public String toString(){
+        return super.toString()+" Mastiles :"+mastiles;
+    }
+
+    @Override
+    public int alquiler(int dias){
+        return (10*dias*eslora*mastiles);
+    }
+}
